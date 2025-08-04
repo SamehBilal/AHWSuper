062ede621797bd418b725f77148682be1bbbac06
Sameh Bilal <sameh.bilal@outlook.com>
2025-08-04 23:46:01 +0300

    updates

--- Modules/Developers/resources/views/livewire/partials/theme/head.blade.php
+++ Modules/Developers/resources/views/livewire/partials/theme/head.blade.php
@@ -36,7 +36,7 @@
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
 @vite(['resources/css/app.css', 'resources/js/app.js'])
-@if(app()->environment('local'))
+@if(app()->environment() == 'local')
     {{-- For local development - use full paths --}}
     @vite(['modules/developers/resources/assets/css/app.css', 'modules/developers/resources/assets/js/app.js'], 'build-developers')
 @else
